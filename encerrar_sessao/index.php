76311716b2bdc44bca81bb5947de9471fdd4bd15
Weverton Pereira Andrade <159034763+Weverton-Anvy@users.noreply.github.com>
2024-02-05 14:55:53 -0400

    Arquivos do Projeto

--- encerrar_sessao/index.php
+++ encerrar_sessao/index.php
@@ -1,1 +1,11 @@
+<?php
+session_start();
 
+// Destroi todas as variáveis de sessão
+session_unset();
+session_destroy();
+
+// Redireciona para a página principal
+header("Location: ../index.php");
+exit();
+?>
